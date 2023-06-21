0d14c7647a0a43295a3a0e45d79d2bb3ce6ed419
NicolasLepore <135086385+NicolasLepore@users.noreply.github.com>
2023-06-20 22:52:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import { learnMoreAnimation } from './app/animations.js';
 
 const learnMoreBtns = document.querySelectorAll('[data-seclearnmore]');
+const image = document.querySelector('[data-container-img]');
 const form = document.querySelector('#form');
 const submitBtn = document.querySelector('#form-submit');
 
-learnMoreBtns.forEach(btn => {
+learnMoreBtns.forEach((btn, i) => {
+
+    btn.style.width = `${image.width}px`;
+
+    if(i == 1) {
+        btn.style.left = `calc(50% - ${image.width}px / 2)`;
+    } 
+
+    if(i == 2) {
+        btn.style.left = `calc(100% - ${image.width}px)`;
+    }
+    
     btn.addEventListener('click', () => {
-        learnMoreAnimation(btn);
+        learnMoreAnimation(btn, image);
     })
 });
 
