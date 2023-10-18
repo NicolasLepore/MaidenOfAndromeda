c1dde6d67bdbdd296610189cde2409c907373986
NicolasLepore <135086385+NicolasLepore@users.noreply.github.com>
2023-10-18 20:04:43 -0300

    Update gallery.js

--- app/gallery.js
+++ app/gallery.js
@@ -1,7 +1,7 @@
 const favorites = document.querySelectorAll("[data-gallery-fav]");
 favorites.forEach(fav => {
     fav.addEventListener('click', (e) => {
-        console.log(e.target.src)
+        //console.log(e.target.src)
         
 
         if(e.target.src === 'http://127.0.0.1:5500/imgs/estrela.png' || e.target.src === 'https://maiden-of-andromeda.vercel.app/imgs/estrela.png') {
