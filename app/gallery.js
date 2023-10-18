36a3a6de682ac219c7aa38968ccfb3fa5bc84e64
NicolasLepore <135086385+NicolasLepore@users.noreply.github.com>
2023-10-18 20:04:11 -0300

    Update gallery.js

--- app/gallery.js
+++ app/gallery.js
@@ -4,7 +4,7 @@ favorites.forEach(fav => {
         console.log(e.target.src)
         
 
-        if(e.target.src === 'http://127.0.0.1:5500/imgs/estrela.png') {
+        if(e.target.src === 'http://127.0.0.1:5500/imgs/estrela.png' || e.target.src === 'https://maiden-of-andromeda.vercel.app/imgs/estrela.png') {
             e.target.src = "../imgs/estrela-amarela.png";
         } else {
             e.target.src = "../imgs/estrela.png";
