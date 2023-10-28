9fb17a9d3c884c4effff5bbcabee7facced1d1ca
NicolasLepore <135086385+NicolasLepore@users.noreply.github.com>
2023-10-28 03:08:08 -0300

    Add files via upload

--- app/gallery.js
+++ app/gallery.js
@@ -1,10 +1,10 @@
 const favorites = document.querySelectorAll("[data-gallery-fav]");
 favorites.forEach(fav => {
     fav.addEventListener('click', (e) => {
-        //console.log(e.target.src)
+        
         
 
-        if(e.target.src === 'http://127.0.0.1:5500/imgs/estrela.png' || e.target.src === 'https://maiden-of-andromeda.vercel.app/imgs/estrela.png') {
+        if(e.target.src === 'http://127.0.0.1:5500/imgs/estrela.png') {
             e.target.src = "../imgs/estrela-amarela.png";
         } else {
             e.target.src = "../imgs/estrela.png";
