9dc3ae7b760973caccc5592263a6f7a46ac55361
NicolasLepore <135086385+NicolasLepore@users.noreply.github.com>
2023-10-28 03:12:45 -0300

    Update gallery.js

--- app/gallery.js
+++ app/gallery.js
@@ -3,7 +3,7 @@ favorites.forEach(fav => {
     fav.addEventListener('click', (e) => {
         
         
-
+        console.log(e.target.src)
         if(e.target.src === 'http://127.0.0.1:5500/imgs/estrela.png') {
             e.target.src = "../imgs/estrela-amarela.png";
         } else {
